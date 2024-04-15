72e894ed42300e99a0fed34ce6dbf84813185d80
HONGJUNWeIO <88540714+HONGJUNWeIO@users.noreply.github.com>
2024-04-15 19:23:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ def get_weather():
     request=post
     re=requests.get(request)
     rep = re.json()
-    return rep."location"
+    return rep[1]
   #  location=rep.location
    # idjs=location.json()
     #id=idjs.id
